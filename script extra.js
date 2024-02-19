519c9f63af1d89efd1205c5654396242afe115f4
Vitoriarochacardoso <128175177+Vitoriarochacardoso@users.noreply.github.com>
2024-02-18 23:59:29 -0300

    Add files via upload

---
+++ script extra.js
@@ -0,0 +1,27 @@
+let nomeUsuario;
+let idadeUsuario;
+let linguagemUsuario;
+let gostoUsuario = 1
+let gostoUsuario = 2
+
+nomeUsuario = prompt("Qual seu nome?");
+
+        alert("Olá " + nomeUsuario);
+
+idadeUsuario = prompt("Quantos anos você tem?");
+
+        alert("Você tem" + idadeUsuario);
+
+linguagemUsuario = prompt("Qual linguagem de programação você esta usando?")
+
+        alert("Já esta aprendendo" + linguagemUsuario);
+
+gostoUsuario = prompt("Você gosta de estudar" + linguagemUsuario? "Responda com o número 1 para SIM ou 2 para NÃO.")
+
+    if(gostoUsuario===1){
+
+        alert(" Muito bom! Continue estudando e você terá muito sucesso.");
+}
+    else(gostoUsuario===2){
+        alert("Ahh que pena... Já tentou aprender outras linguagens?");
+    }
