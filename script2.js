faae0f41527c96857028141be77eb971c2d57fa9
Vitoriarochacardoso <128175177+Vitoriarochacardoso@users.noreply.github.com>
2024-02-18 23:46:04 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,15 @@
+let nomeUsuario;
+let idadeUsuario;
+let linguagemUsuario;
+
+nomeUsuario = prompt("Qual seu nome?");
+
+alert("Olá " + nomeUsuario);
+
+idadeUsuario = prompt("Quantos anos você tem?");
+
+alert("Você tem" + idadeUsuario);
+
+linguagemUsuario = prompt("Qual linguagem de programação você esta usando?")
+
+alert("Já esta aprendendo" + linguagemUsuario);
